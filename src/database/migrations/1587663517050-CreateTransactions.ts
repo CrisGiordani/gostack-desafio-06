8e40aea565083c7e2d524c0d568d6aa0d1daa0b5
Cristian Giordani <cristian@cuby.com.br>
2020-04-23 14:49:15 -0300

    models & migrations

---
+++ src/database/migrations/1587663517050-CreateTransactions.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTransactions1587660459548
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'value',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'category_id',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('transactions');
+  }
+}
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
@@ -7,12 +10,26 @@ import { Router } from 'express';
 
 const transactionsRouter = Router();
 
+const transactionsRepository = new TransactionsRepository();
+
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactions = transactionsRepository.all();
+  const balance = transactionsRepository.getBalance();
+
+  const completeInfo = {
+    transactions,
+    balance,
+  };
+  return response.json(completeInfo);
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type } = request.body;
+  const createdTransaction = new CreateTransactionService(
+    transactionsRepository,
+  );
+  const transaction = createdTransaction.execute({ title, value, type });
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
